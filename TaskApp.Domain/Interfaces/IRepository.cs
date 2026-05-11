268d9c3f7ccbe7d454ee0201859b5d11ad12ac1b
Jorge Peralta <jpse110507@gmail.com>
2026-05-11 10:34:49 +0200

    feat: add IRepository interface

---
+++ TaskApp.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+namespace TaskApp.Domain.Interfaces;
+
+public interface IRepository<T>
+{
+  public Task<IEnumerable<T>> GetAll();
+  public Task<T?> GetById(int id);
+  public Task Add(T entity);
+  public Task Delete(int id);
+
+}
